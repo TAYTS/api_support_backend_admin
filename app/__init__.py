2c660b8a140ec42fe2bc156516394e065a2b1539
TAY TS <taytzushieh@gmail.com>
2019-04-06 19:03:10 +0800

    feature: Remove user module

--- app/__init__.py
+++ app/__init__.py
@@ -31,11 +31,9 @@ def make_app(config='config.py'):
     JWTManager(app)
 
     # Degine all the moduls
-    from app.modules import users
     from app.modules import tickets
 
     # Register the blueprint of each module
-    app.register_blueprint(users.module)
     app.register_blueprint(tickets.module)
 
     return app
